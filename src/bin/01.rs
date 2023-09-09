fd47d424bbae0946c8e5aaddfa7ff7fa206b0252
Eric Barr <eb@eb2.co>
2023-09-09 08:07:21 -0700

    day 01 part 1 solution

---
+++ src/bin/01.rs
@@ -0,0 +1,33 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    let up_count = input.chars().filter(|&c| c == '(').count() as u32;
+    let down_count = input.chars().filter(|&c| c == ')').count() as u32;
+
+    Some(up_count - down_count)
+}
+
+pub fn part_two(_input: &str) -> Option<u32> {
+    None
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 1);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_one(&input), Some(0));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_two(&input), None);
+    }
+}
