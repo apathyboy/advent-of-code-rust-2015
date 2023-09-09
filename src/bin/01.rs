d25fd2263caea9cd7fee18c836ccb110a390d0e2
Eric Barr <eb@eb2.co>
2023-09-09 12:46:13 -0700

    day 01 part 2 solution

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,12 +1,30 @@
-pub fn part_one(input: &str) -> Option<u32> {
-    let up_count = input.chars().filter(|&c| c == '(').count() as u32;
-    let down_count = input.chars().filter(|&c| c == ')').count() as u32;
+pub fn part_one(input: &str) -> Option<i32> {
+    let up_count = input.chars().filter(|&c| c == '(').count() as i32;
+    let down_count = input.chars().filter(|&c| c == ')').count() as i32;
 
     Some(up_count - down_count)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut sum = 0;
+
+    input
+        .chars()
+        .map(|c| match c {
+            '(' => 1,
+            ')' => -1,
+            _ => panic!("Unexpected character"),
+        })
+        .enumerate()
+        .find_map(|(idx, x)| {
+            sum += x;
+
+            if sum == -1 {
+                Some(idx as u32 + 1)
+            } else {
+                None
+            }
+        })
 }
 
 fn main() {
@@ -22,12 +40,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_one(&input), Some(0));
+        assert_eq!(part_one(&input), Some(-1));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(3));
     }
 }
