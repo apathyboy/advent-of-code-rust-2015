e3d3784455f26adda2271c88d8c279e9f7fe2d84
eb <eb@eb2.co>
2023-10-14 17:41:19 -0700

    day 05 solutions

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,9 +1,42 @@
-pub fn part_one(_input: &str) -> Option<u32> {
-    None
+pub fn contains_vowels(s: &str) -> bool {
+    s.chars().filter(|c| "aeiou".contains(*c)).count() >= 3
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn contains_double_letter(s: &str) -> bool {
+    s.chars().zip(s.chars().skip(1)).any(|(a, b)| a == b)
+}
+
+pub fn contains_naughty_strings(s: &str) -> bool {
+    s.contains("ab") || s.contains("cd") || s.contains("pq") || s.contains("xy")
+}
+
+pub fn contains_repeated_pair(s: &str) -> bool {
+    s.chars()
+        .zip(s.chars().skip(1))
+        .enumerate()
+        .any(|(i, (a, b))| s[i + 2..].contains(&format!("{}{}", a, b)))
+}
+
+pub fn contains_repeated_letter(s: &str) -> bool {
+    s.chars().zip(s.chars().skip(2)).any(|(a, b)| a == b)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    input
+        .lines()
+        .filter(|s| contains_vowels(s) && contains_double_letter(s) && !contains_naughty_strings(s))
+        .count()
+        .try_into()
+        .ok()
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    input
+        .lines()
+        .filter(|s| contains_repeated_pair(s) && contains_repeated_letter(s))
+        .count()
+        .try_into()
+        .ok()
 }
 
 fn main() {
@@ -19,12 +52,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 5);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(2));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 5);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(2));
     }
 }
