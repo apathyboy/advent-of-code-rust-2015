4f453da9b2f1d9f21046e007982de302d8e089a0
eb <eb@eb2.co>
2023-10-22 16:54:31 -0700

    day 09 solutions

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,9 +1,98 @@
-pub fn part_one(_input: &str) -> Option<u32> {
-    None
+use itertools::Itertools;
+use std::collections::HashMap;
+
+fn parse_distance(line: &str) -> Option<((&str, &str), u32)> {
+    let mut parts = line.split(' ');
+    let from = parts.next()?;
+    let to = parts.nth(1)?;
+    let distance = parts.nth(1)?.parse::<u32>().ok()?;
+    Some(((from, to), distance))
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+fn generate_permutations<T: Clone>(items: &[T]) -> Vec<Vec<T>> {
+    if items.len() == 1 {
+        return vec![items.to_vec()];
+    }
+
+    let mut permutations = Vec::new();
+    for (i, item) in items.iter().enumerate() {
+        let mut items = items.to_vec();
+        items.remove(i);
+        let mut sub_permutations = generate_permutations(&items);
+        for sub_permutation in &mut sub_permutations {
+            sub_permutation.insert(0, item.clone());
+        }
+        permutations.append(&mut sub_permutations);
+    }
+    permutations
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let distances = input
+        .lines()
+        .map(parse_distance)
+        .collect::<Option<HashMap<(&str, &str), u32>>>()?;
+    let cities = distances
+        .keys()
+        .flat_map(|(from, to)| vec![*from, *to])
+        .unique()
+        .collect_vec();
+
+    let mut min_distance = u32::max_value();
+
+    for permutation in generate_permutations(&cities) {
+        let mut distance = 0;
+        for i in 0..permutation.len() - 1 {
+            if distances.contains_key(&(permutation[i], permutation[i + 1])) {
+                distance += distances
+                    .get(&(permutation[i], permutation[i + 1]))
+                    .unwrap();
+            } else {
+                distance += distances
+                    .get(&(permutation[i + 1], permutation[i]))
+                    .unwrap();
+            }
+        }
+        if distance < min_distance {
+            min_distance = distance;
+        }
+    }
+
+    Some(min_distance)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let distances = input
+        .lines()
+        .map(parse_distance)
+        .collect::<Option<HashMap<(&str, &str), u32>>>()?;
+    let cities = distances
+        .keys()
+        .flat_map(|(from, to)| vec![*from, *to])
+        .unique()
+        .collect_vec();
+
+    let mut max_distance = 0;
+
+    for permutation in generate_permutations(&cities) {
+        let mut distance = 0;
+        for i in 0..permutation.len() - 1 {
+            if distances.contains_key(&(permutation[i], permutation[i + 1])) {
+                distance += distances
+                    .get(&(permutation[i], permutation[i + 1]))
+                    .unwrap();
+            } else {
+                distance += distances
+                    .get(&(permutation[i + 1], permutation[i]))
+                    .unwrap();
+            }
+        }
+        if distance > max_distance {
+            max_distance = distance;
+        }
+    }
+
+    Some(max_distance)
 }
 
 fn main() {
@@ -19,12 +108,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 9);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(605));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 9);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(982));
     }
 }
