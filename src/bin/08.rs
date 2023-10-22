cf5d4521699a06d9f5b187cb2150f9c2d1027b09
eb <eb@eb2.co>
2023-10-22 14:19:28 -0700

    day 08 solutions

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,9 +1,87 @@
-pub fn part_one(_input: &str) -> Option<u32> {
-    None
+fn count_chars(input: &str) -> (usize, usize) {
+    let mut chars = 0;
+    let mut mem = 0;
+    let mut iter = input.chars();
+    while let Some(c) = iter.next() {
+        chars += 1;
+        match c {
+            '\\' => match iter.next() {
+                Some('\\') => {
+                    mem += 1;
+                    chars += 1;
+                }
+                Some('\"') => {
+                    mem += 1;
+                    chars += 1;
+                }
+                Some('x') => {
+                    iter.next();
+                    iter.next();
+                    mem += 1;
+                    chars += 3;
+                }
+                _ => {}
+            },
+            '\"' => {}
+            _ => {
+                mem += 1;
+            }
+        }
+    }
+    (chars, mem)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+fn encode_chars(input: &str) -> (usize, usize) {
+    let mut chars = 0;
+    let mut mem = 2;
+    let mut iter = input.chars();
+    while let Some(c) = iter.next() {
+        chars += 1;
+        match c {
+            '\\' => match iter.next() {
+                Some('\\') => {
+                    mem += 4;
+                    chars += 1;
+                }
+                Some('\"') => {
+                    mem += 4;
+                    chars += 1;
+                }
+                Some('x') => {
+                    iter.next();
+                    iter.next();
+                    mem += 5;
+                    chars += 3;
+                }
+                _ => {}
+            },
+            '\"' => {
+                mem += 2;
+            }
+            _ => {
+                mem += 1;
+            }
+        }
+    }
+    (chars, mem)
+}
+
+pub fn part_one(_input: &str) -> Option<usize> {
+    _input
+        .lines()
+        .map(count_chars)
+        .fold(Some(0), |acc, (chars, mem)| {
+            acc.and_then(|acc| Some(acc + chars - mem))
+        })
+}
+
+pub fn part_two(_input: &str) -> Option<usize> {
+    _input
+        .lines()
+        .map(encode_chars)
+        .fold(Some(0), |acc, (chars, mem)| {
+            acc.and_then(|acc| Some(acc + mem - chars))
+        })
 }
 
 fn main() {
