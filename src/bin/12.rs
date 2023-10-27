5cd6ec6796f42a7583b4e821fc16aaed7208e045
eb <eb@eb2.co>
2023-10-27 10:09:38 -0700

    day 12 part 1 solution

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,8 +1,20 @@
-pub fn part_one(_input: &str) -> Option<u32> {
-    None
+use regex::Regex;
+use std::str::FromStr;
+
+fn parse_numbers(input: &str) -> i32 {
+    let re = Regex::new(r"([+-]?\d+)").unwrap();
+    re.captures_iter(input)
+        .map(|cap| i32::from_str(&cap[1]).unwrap())
+        .sum()
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<i32> {
+    let result = input.lines().map(|line| parse_numbers(line)).sum();
+
+    Some(result)
+}
+
+pub fn part_two(_input: &str) -> Option<i32> {
     None
 }
 
@@ -19,12 +31,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 12);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(6));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 12);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(4));
     }
 }
