5bb3829ccbfcbd05980ad91dc593988cc17f48da
eb <eb@eb2.co>
2023-10-28 11:38:00 -0700

    day 12 part 2 solution

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -8,14 +8,60 @@ fn parse_numbers(input: &str) -> i32 {
         .sum()
 }
 
-pub fn part_one(input: &str) -> Option<i32> {
-    let result = input.lines().map(|line| parse_numbers(line)).sum();
+fn find_open_bracket_from_position(input: &str, position: usize) -> usize {
+    let mut open_brackets = 0;
+    let mut index = position;
+    loop {
+        if input.chars().nth(index).unwrap() == '}' {
+            open_brackets += 1;
+        } else if input.chars().nth(index).unwrap() == '{' {
+            open_brackets -= 1;
+            if open_brackets < 0 {
+                break;
+            }
+        }
+        index -= 1;
+    }
+    index
+}
 
-    Some(result)
+fn find_close_bracket_from_position(input: &str, position: usize) -> usize {
+    let mut close_brackets = 0;
+    let mut index = position;
+    loop {
+        if input.chars().nth(index).unwrap() == '{' {
+            close_brackets += 1;
+        } else if input.chars().nth(index).unwrap() == '}' {
+            close_brackets -= 1;
+            if close_brackets < 0 {
+                break;
+            }
+        }
+        index += 1;
+    }
+    index
+}
+
+fn remove_red_objects(input: &str) -> String {
+    match input.find(":\"red\"") {
+        Some(index) => {
+            let begin_index = find_open_bracket_from_position(input, index);
+            let end_index = find_close_bracket_from_position(input, index);
+            let mut new_input = input.to_string();
+            new_input.replace_range(begin_index..=end_index, "");
+            remove_red_objects(&new_input)
+        }
+        _ => input.to_string(),
+    }
+}
+
+pub fn part_one(input: &str) -> Option<i32> {
+    Some(parse_numbers(input))
 }
 
 pub fn part_two(_input: &str) -> Option<i32> {
-    None
+    let input = remove_red_objects(_input);
+    Some(parse_numbers(&input))
 }
 
 fn main() {
