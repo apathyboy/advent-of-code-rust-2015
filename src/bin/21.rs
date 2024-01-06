c6a7ad519ce9f8bac0e53fb960a79a459dd0cf2a
eb <eb@eb2.co>
2024-01-05 17:08:37 -0700

    Day 21 part 2 solution

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -2,8 +2,8 @@ use itertools::Itertools;
 
 advent_of_code::solution!(21);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let (boss_hit_points, boss_damage, boss_armor) = input.lines().collect_tuple().unwrap();
+fn parse_boss(input: &str) -> Option<(i32, i32, i32)> {
+    let (boss_hit_points, boss_damage, boss_armor) = input.lines().collect_tuple()?;
     let boss_hit_points = boss_hit_points
         .split_whitespace()
         .nth(2)
@@ -23,6 +23,14 @@ pub fn part_one(input: &str) -> Option<u32> {
         .parse::<i32>()
         .unwrap();
 
+    Some((boss_hit_points, boss_damage, boss_armor))
+}
+
+fn create_store() -> (
+    [(u32, i32, i32); 5],
+    [(u32, i32, i32); 6],
+    [(u32, i32, i32); 8],
+) {
     let weapons_store = [(8, 4, 0), (10, 5, 0), (25, 6, 0), (40, 7, 0), (74, 8, 0)];
     let armor_store = [
         (0, 0, 0),
@@ -42,6 +50,13 @@ pub fn part_one(input: &str) -> Option<u32> {
         (40, 0, 2),
         (80, 0, 3),
     ];
+
+    (weapons_store, armor_store, rings_store)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (boss_hit_points, boss_damage, boss_armor) = parse_boss(input)?;
+    let (weapons_store, armor_store, rings_store) = create_store();
 
     let mut min_cost = u32::MAX;
 
@@ -76,8 +91,41 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(min_cost)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let (boss_hit_points, boss_damage, boss_armor) = parse_boss(input)?;
+    let (weapons_store, armor_store, rings_store) = create_store();
+
+    let mut max_cost = 0;
+
+    for weapon in weapons_store {
+        for armor in armor_store.iter().combinations(1) {
+            for rings in rings_store.iter().combinations(2) {
+                let cost = weapon.0 + armor[0].0 + rings[0].0 + rings[1].0;
+                let damage: i32 = weapon.1 + armor[0].1 + rings[0].1 + rings[1].1;
+                let armor: i32 = weapon.2 + armor[0].2 + rings[0].2 + rings[1].2;
+
+                let mut boss_hp: i32 = boss_hit_points;
+                let mut player_hp: i32 = 100;
+
+                loop {
+                    boss_hp = boss_hp.saturating_sub(damage.saturating_sub(boss_armor));
+                    if boss_hp <= 0 {
+                        break;
+                    }
+                    player_hp = player_hp.saturating_sub(boss_damage.saturating_sub(armor));
+                    if player_hp <= 0 {
+                        break;
+                    }
+                }
+
+                if player_hp <= 0 && cost > max_cost {
+                    max_cost = cost;
+                }
+            }
+        }
+    }
+
+    Some(max_cost)
 }
 
 #[cfg(test)]
@@ -85,14 +133,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+    fn test_can_parse_boss() {
+        let result = parse_boss(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some((12, 7, 2)));
     }
 }
